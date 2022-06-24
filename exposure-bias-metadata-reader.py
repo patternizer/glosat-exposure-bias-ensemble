d2e80b6de48cce30e5d9221035736c737360f489
patternizer <michael.a.taylor@uea.ac.uk>
2022-06-24 16:25:23 +0100

    Latest version of exposure bias corrections

--- exposure-bias-metadata-reader.py
+++ exposure-bias-metadata-reader.py
@@ -20,7 +20,6 @@ from datetime import datetime
 # Plotting libraries:
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
-
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
@@ -28,17 +27,15 @@ import seaborn as sns; sns.set()
 #------------------------------------------------------------------------------
 
 fontsize = 16
-use_exposure_category = True    # (default=True) False --> use exposure
-use_nma_assumption = True      # (default=False) True --> use all source_codes
-use_transition = True          # (default=False) True --> include transitions in categories
+use_nma_assumption = False     # (default=False) True --> use all source_codes
+use_transition = True          # (default=True) False --> include transitions in categories
 
 #------------------------------------------------------------------------------
 # LOAD: Emily's raw data
 #------------------------------------------------------------------------------
 
-metadatafile = 'DATA/GloSATPrelim04StationData_Exposure_DataforMTaylor.csv'
-
-df_metadata = pd.read_csv( metadatafile ) 
+#metadatafile = 'DATA/GloSATPrelim04StationData_Exposure_DataforMTaylor.csv'
+#df_metadata = pd.read_csv( metadatafile ) 
 
 #Index(['year', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
 #       'stationcode', 'stationlat', 'stationlon', 'stationelevation',
@@ -47,18 +44,23 @@ df_metadata = pd.read_csv( metadatafile )
 #       'exposure_category', 'source_flag', 'exposurecorrected_flag'],
 #      dtype='object')
 
-columns = [ 'year', 'stationcode', 'stationlat', 'stationlon', 'stationelevation', 'exposure', 'exposure_category', 'source_flag', 'exposurecorrected_flag' ]
+metadatafile = 'DATA/df_exposure_bias.pkl'
+df_metadata = pd.read_pickle( metadatafile, compression='bz2' )
+
+#Index(['datetime', 'stationcode', 'exposure_category', 'bias', 'uncertainty', 'source_flag', 'exposurecorrected_flag'],
+#      dtype='object')
+
+columns = [ 'datetime', 'stationcode', 'exposure_category', 'source_flag', 'exposurecorrected_flag' ]
 df = pd.DataFrame(df_metadata, columns=columns)
     
 #------------------------------------------------------------------------------
 # STATS:
 #------------------------------------------------------------------------------
 
-t_start = df.year.min()                                                                 # 1658
-t_end = df.year.max()                                                                   # 2021
-n_stations = df.stationcode.unique().shape[0]                                           # 11873
-n_exposure = df.exposure.unique().shape[0]                                              # 130 ( see below )
-n_exposure_category = df.exposure_category.unique().shape[0]                            #  14 ( see below )
+t_start = df.datetime.dt.year.min()                                                     # 1781
+t_end = df.datetime.dt.year.max()                                                       # 2021
+n_stations = df.stationcode.unique().shape[0]                                           # 4404
+n_exposure_category = df.exposure_category.unique().shape[0]                            #  15 ( see below )
 n_source_flag = df.source_flag.unique().shape[0]                                        #   4 [ nan, 1., 2., 3. ]
 n_exposurecorrected_flag = df.exposurecorrected_flag.unique().shape[0]                  #   2 [ nan, 1. ]
 n_exposurecorrected = df[df.exposurecorrected_flag==1].stationcode.unique().shape[0]    #  21
@@ -76,33 +78,17 @@ if use_transition == False: df = df[ df.exposure_category.str.contains('Transiti
 
 # CREATE: dictionary mapping exposure_category keys to integer values
 
-if use_exposure_category == True:
+dg = df.copy().dropna(subset=['exposure_category','source_flag']).reset_index(drop=True)
+exposure_category_dict = dict( zip( dg.exposure_category.unique(), np.arange(n_exposure_category) ) )
 
-    dg = df.copy().dropna(subset=['exposure_category','source_flag']).reset_index(drop=True)
-    exposure_category_dict = dict( zip( dg.exposure_category.unique(), np.arange(n_exposure_category) ) )
+# APPLY: dict map to dataframe
 
-    # APPLY: dict map to dataframe
+dg['exposure_category'] = dg['exposure_category'].map( exposure_category_dict )
 
-    dg['exposure_category'] = dg['exposure_category'].map( exposure_category_dict )
-
-    # FIND: break indices
+# FIND: break indices
     
-    v = dg['exposure_category']
-    t = np.where( np.diff(v) )[0] + 1  
-
-else:
-
-    dg = df.copy().dropna(subset=['exposure','source_flag']).reset_index(drop=True)
-    exposure_dict = dict( zip( dg.exposure.unique(), np.arange(n_exposure) ) )
-
-    # APPLY: dict map to dataframe
-
-    dg['exposure'] = dg['exposure'].map( exposure_dict )
-
-    # FIND: break indices
-    
-    v = dg['exposure']
-    t = np.where( np.diff(v) )[0] + 1  
+v = dg['exposure_category']
+t = np.where( np.diff(v) )[0] + 1  
 
 # EXTRACT: breaks subset
 
@@ -116,27 +102,39 @@ dh = dh.drop( mask )
 
 # PLOT: breakpoints from exposure changes ( x=stationcode, y=breakpoint year )
 
+assumptionstr = ''
+transitionstr = ''
+if use_nma_assumption == True: assumptionstr = '_nma_assumption'
+if use_transition == True: transitionstr = '_transition'
+figstrstem = 'breakpoints_exposure_category_source_code_stationcode'
+figstr = figstrstem + assumptionstr + transitionstr + '.png'
+
+
 fig, ax = plt.subplots(figsize=(12, 7))
 for i in range( dh.source_flag.unique().shape[0] ):    
     da = dh[ dh.source_flag == i+1 ]
-    plt.plot(da.stationcode, da.year, 'o', markersize=5, alpha=0.5, label='source_code='+str(i+1)+' : median='+str( int( np.median( da.year ) ) ) )
+#    plt.plot(da.stationcode, da.datetime.dt.year, 'o', markersize=5, alpha=0.5, label='source_code='+str(i+1)+' : median='+str( int( np.median( da.datetime.dt.year ) ) ) )
+    plt.plot(da.datetime.dt.year, 'o', markersize=5, alpha=0.5, label='source_code='+str(i+1)+' : median='+str( int( np.median( da.datetime.dt.year ) ) ) )
 ax.axes.xaxis.set_ticklabels([])
 plt.legend(loc='lower right', fontsize=fontsize)
 plt.xlabel('stationcode', fontsize=fontsize)
 plt.ylabel('breakpoint year', fontsize=fontsize)
-if use_exposure_category == True:
-    plt.title( 'Breakpoints from changes in exposure_category', fontsize=fontsize )
-    plt.savefig( 'breakpoints_exposure_category_source_code_stationcode.png', dpi=300 )
-else:
-    plt.title( 'Breakpoints from changes in exposure', fontsize=fontsize )
-    plt.savefig( 'breakpoints_exposure_source_code_stationcode.png', dpi=300 )
+plt.title( 'Breakpoints from changes in exposure_category', fontsize=fontsize )
+plt.savefig( figstr, dpi=300 )
 
 # PLOT: distribution of breakpoint years by source_code
+
+assumptionstr = ''
+transitionstr = ''
+if use_nma_assumption == True: assumptionstr = '_nma_assumption'
+if use_transition == True: transitionstr = '_transition'
+figstrstem = 'breakpoints_exposure_category_source_code_violinplot'
+figstr = figstrstem + assumptionstr + transitionstr + '.png'
 
 fig, ax = plt.subplots(figsize=(12, 7))
 for i in range( dh.source_flag.unique().shape[0] ):
     da = dh[ dh.source_flag == i+1 ]
-    ax.violinplot(da.year, positions = [i+1])
+    ax.violinplot(da.datetime.dt.year, positions = [i+1])
 ax.set_xlim(0.5, dh.source_flag.unique().shape[0] + 0.5)
 positions   = [1, 2, 3]
 labels = ['1','2','3']
@@ -144,28 +142,24 @@ ax.set_xticks(positions)
 ax.set_xticklabels(labels)
 plt.xlabel('source_code', fontsize=fontsize)
 plt.ylabel('breakpoint year', fontsize=fontsize)
-if use_exposure_category == True:
-    plt.title( 'Breakpoints from changes in exposure_category', fontsize=fontsize )
-    plt.savefig( 'breakpoints_exposure_category_source_code_violinplot.png', dpi=300 )
-else:
-    plt.title( 'Breakpoints from changes in exposure', fontsize=fontsize )
-    plt.savefig( 'breakpoints_exposure_source_code_violinplot.png', dpi=300 )
+plt.title( 'Breakpoints from changes in exposure_category', fontsize=fontsize )
+plt.savefig( figstr, dpi=300 )
 
 #==============================================================================
 # SAVE: breakpoints dataframe
 #==============================================================================
 
-dh.to_pickle('df_breaks.pkl', compression='bz2')
+assumptionstr = ''
+transitionstr = ''
+if use_nma_assumption == True: assumptionstr = '_nma_assumption'
+if use_transition == True: transitionstr = '_transition'
+pklstem = 'df_breaks'
+pklfile = pklstem + assumptionstr + transitionstr + '.pkl'
+
+dh.to_pickle( pklfile, compression='bz2')
 
 #------------------------------------------------------------------------------
 print('** END')
-
-
-
-
-
-
-
 
 #------------------------------------------------------------------------------
 # exposurecorrected_flag A-Z: (float64)
@@ -216,98 +210,6 @@ print('** END')
 #       'Transition Montsouris', 'Transition Stevenson-type screen', 'Transition Wild hut', 
 #       'Unknown', 
 #       'Wall'], dtype='<U32')
-
-#------------------------------------------------------------------------------
-# exposure A-Z: (object)
-#------------------------------------------------------------------------------
-        
-# np.sort( list(df.exposure.unique()) )
-#
-# array(['nan', 
-#       'AMJJASO: Hazen shelter; NDJFM: Wall', 'AWS', 'Balcony', 'Bilham screen', 'CRS', 'CRS Roof', 
-#       'Canadian thermometer shelter', 'Canvas tent on roof', 'Double screen', 'Double screen Roof',
-#       'E Wall', 'E wall (no screen)', 'ENE window', 'East Wall', 'East wall', 'East window shelter',
-#       'Eiffel Shelter adapted for the Tropics', 'French screen',
-#      
-#       'Glaisher', 
-#       'Glaisher stand', 
-#       'Glaisher stand in Summerhouse',
-#       'Glaisher/Wall', 
-#       
-#       'Greenwich stand', 'Indian Pattern Thermometer shed', 'Indian Thatched Shelter',
-#
-#       'Inside and unheated room', 
-#       'Inside glass gallery',
-#       'Inside large room', 
-#       'Inside small house', 
-#       'Inside small room',
-#       
-#       'Large Octogonal screen', 'Large louvred screen', 'Lawson shelter', 'Louvred Octogon (Summerhouse)', 
-#       'Louvred shed', 'MMTS', 'Metal cylindrical housing N window', 'Metal screen', 'Modified Greenwich stand', 
-#       
-#       'Montsouris',
-#       
-#       'N balcony wickerwork housing', 'N wall (no screen)', 'N window', 'N window (no screen)', 'NE Wall (no screen)', 
-#       'NE window', 'NE window shelter', 'NNW window', 'NNW window shelter', 'NW Window shelter', 
-#       'NW wall', 'NW window shelter', 'North Porch',
-#
-#       'North Wall', 
-#       'North Wall (cylindrical metal shield)',
-#       'North Wall (no screen)', 
-#       'North Wall (unscreened)',
-#       'North Window', 'North Window shelter', 'North wall',
-#       'North wall (zinc cylindrical screen)', 'North window',
-#       'North window shelter', 'North window shelter influenced by stove',
-#
-#       'Open',
-#
-#       'Pavillion on roof with thermometer inside in zinc housing', 'Porch', 'Porch wall', 'Radiation shield', 
-#       'Railroad shelter', 'SE Balcony - poor exposure', 'SE Verandah', 'SS at N Wall', 'SW Window screen', 
-#       'Shade of a tree', 'Shelter on tower terrace', 'Smithsonian window shelter, near fireplace and chimney',
-#       'South porch, inside on sunny days', 'South wall', 'South window',
-#
-#       'Stevenson screen', 
-#       'Stevenson screen Roof',
-#       'Stevenson type screen', 
-#       'Stevenson type screen 2nd floor Terrace',
-#       'Stevenson type screen Roof',
-#       'Stevenson type screen Roof (poor exposure)',
-#       'Stevenson type screen Roof close to chimney',
-#       'Stevenson type screen Roof close to steam vent',
-#       'Stevenson-type screen', 
-#       'Stevensone screen', 
-#
-#       'Summerhouse', 'Thermometer hut on roof', 'Thermometer shed', 'Tin housing on North wall', 'Tin shelter at N window',
-#
-#       'Transition Montsouris', 
-#       'Transition Stevenson-type screen',
-#       'Transition Wild hut', 
-#
-#       'Trestle', 'Under piazza', 'Unknown', 'Unstandardised', 'Verandah', 
-#
-#       'Wall', 
-#       'Wall (no screen)',
-#       'Wall mounted tin housing, influenced by radiation', 
-#       'Wall screen',
-#       'Wall shelter', 
-#
-#       'Wild hut',
-#       'Wild hut not according to instructions',
-#       'Wild shield and hut at NNE window', 
-#       'Wild shield at North window',
-#
-#       'Window', 
-#       'Window (no screen)', 
-#       'Window in porch',
-#       'Window mounted wild screen and hut',
-#       'Window of unheated building', 
-#       'Window shelter',
-#
-#       'Zinc housing NE window', 'Zinc housing and Wild hut at N window', 'Zinc housing at N window', 
-#       'Zinc sheet housing at NNE wall', 'Zinc sheet housing in wood hut mounted on N Wall',
-#       'Zinc sheeting in garden', 'Zinc sheild inside canvas covered box',
-#       'Zinc shield and Wild hut mounted at window in porch', 'Zinc shield and Wild shelter at window',
-#       'Zinc shield inside Wild hut on NE wall', 'zinc sheet housing on 2nd floor balcony'], dtype='<U57')    
 #------------------------------------------------------------------------------
 
 
--- exposure-bias-model-reader.py
+++ exposure-bias-model-reader.py
@@ -1,8 +1,8 @@
 #------------------------------------------------------------------------------
 # PROGRAM: exposure-bias-model-reader.py
 #------------------------------------------------------------------------------
-# Version 0.3
-# 16 June, 2022
+# Version 0.4
+# 23 June, 2022
 # Michael Taylor
 # https://patternizer.github.io
 # michael DOT a DOT taylor AT uea DOT ac DOT uk
@@ -23,13 +23,12 @@ from datetime import datetime
 # SETTINGS: 
 #------------------------------------------------------------------------------
 
-modelfile1 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part1_GloSAT_16.06.22.csv'
-modelfile2 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part2_GloSAT_16.06.22.csv'
-modelfile3 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part3_GloSAT_16.06.22.csv'
-modelfile4 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part4_GloSAT_16.06.22.csv'
-modelfile5 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part5_GloSAT_16.06.22.csv'
-modelfile6 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part6_GloSAT_16.06.22.csv'
-modelfile7 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part7_GloSAT_16.06.22.csv'
+modelfile1 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part1_GloSAT_22.06.22.csv'
+modelfile2 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part2_GloSAT_22.06.22.csv'
+modelfile3 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part3_GloSAT_22.06.22.csv'
+modelfile4 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part4_GloSAT_22.06.22.csv'
+modelfile5 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part5_GloSAT_22.06.22.csv'
+modelfile6 = 'DATA/GloSATP04_Extratropics_ExposureBias_v0.4_Part6_GloSAT_22.06.22.csv'
 
 exposure_bias_model_file = 'df_exposure_bias.pkl'
 
@@ -43,7 +42,6 @@ df3 = pd.read_csv( modelfile3 )
 df4 = pd.read_csv( modelfile4 ) 
 df5 = pd.read_csv( modelfile5 ) 
 df6 = pd.read_csv( modelfile6 ) 
-df7 = pd.read_csv( modelfile7 ) 
 
 # Index(['index', 'stationcode', 'exposure_category', 'bias_estimate', 'bias_estimate_2.5', 'bias_estimate_97.5', 'source_flag', 'exposurecorrected_flag'], dtype='object')
 
@@ -59,7 +57,6 @@ datetimes3 = df3[ df3.columns[0] ]
 datetimes4 = df4[ df4.columns[0] ]
 datetimes5 = df5[ df5.columns[0] ]
 datetimes6 = df6[ df6.columns[0] ]
-datetimes7 = df7[ df7.columns[0] ]
 
 datetimes1_standardized = list( pd.to_datetime(datetimes1, format='%Y-%m-%d') )
 datetimes2_standardized = list( pd.to_datetime(datetimes2, format='%Y-%m-%d') )
@@ -67,9 +64,8 @@ datetimes3_standardized = list( pd.to_datetime(datetimes3, format='%Y-%m-%d') )
 datetimes4_standardized = list( pd.to_datetime(datetimes4, format='%Y-%m-%d') )
 datetimes5_standardized = list( pd.to_datetime(datetimes5, format='%Y-%m-%d') )
 datetimes6_standardized = list( pd.to_datetime(datetimes6, format='%Y-%m-%d') )
-datetimes7_standardized = list( pd.to_datetime(datetimes7, format='%Y-%m-%d') )
 
-datetimes = datetimes1_standardized + datetimes2_standardized + datetimes3_standardized + datetimes4_standardized + datetimes5_standardized + datetimes6_standardized + datetimes7_standardized
+datetimes = datetimes1_standardized + datetimes2_standardized + datetimes3_standardized + datetimes4_standardized + datetimes5_standardized + datetimes6_standardized
 
 # CONCATENATE: stationcodes list [ NB: int64 --> 6-digit str ]
 
@@ -79,7 +75,6 @@ stationcodes3 = df3[ df3.columns[1] ]
 stationcodes4 = df4[ df4.columns[1] ]
 stationcodes5 = df5[ df5.columns[1] ]
 stationcodes6 = df6[ df6.columns[1] ]
-stationcodes7 = df7[ df7.columns[1] ]
 
 stationcodes1_6digit = [ str(stationcodes1[i]).zfill(6) for i in range(len(stationcodes1)) ]
 stationcodes2_6digit = [ str(stationcodes2[i]).zfill(6) for i in range(len(stationcodes2)) ]
@@ -87,9 +82,8 @@ stationcodes3_6digit = [ str(stationcodes3[i]).zfill(6) for i in range(len(stati
 stationcodes4_6digit = [ str(stationcodes4[i]).zfill(6) for i in range(len(stationcodes4)) ]
 stationcodes5_6digit = [ str(stationcodes5[i]).zfill(6) for i in range(len(stationcodes5)) ]
 stationcodes6_6digit = [ str(stationcodes6[i]).zfill(6) for i in range(len(stationcodes6)) ]
-stationcodes7_6digit = [ str(stationcodes7[i]).zfill(6) for i in range(len(stationcodes7)) ]
 
-stationcodes = stationcodes1_6digit + stationcodes2_6digit + stationcodes3_6digit + stationcodes4_6digit + stationcodes5_6digit + stationcodes6_6digit + stationcodes7_6digit 
+stationcodes = stationcodes1_6digit + stationcodes2_6digit + stationcodes3_6digit + stationcodes4_6digit + stationcodes5_6digit + stationcodes6_6digit 
 
 # CONCATENATE: exposure_category list --> used to create dict for integer changepoint flag
 
@@ -99,9 +93,8 @@ exposure_category3 = list( df3[ df3.columns[2] ] )
 exposure_category4 = list( df4[ df4.columns[2] ] )
 exposure_category5 = list( df5[ df5.columns[2] ] )
 exposure_category6 = list( df6[ df6.columns[2] ] )
-exposure_category7 = list( df7[ df7.columns[2] ] )
 
-exposure_categories = exposure_category1 + exposure_category2 + exposure_category3 + exposure_category4 + exposure_category5 + exposure_category6 + exposure_category7
+exposure_categories = exposure_category1 + exposure_category2 + exposure_category3 + exposure_category4 + exposure_category5 + exposure_category6
 
 # CONCATENATE: bias estimates
 
@@ -111,9 +104,8 @@ bias_estimates3 = list( df3[ df3.columns[3] ] )
 bias_estimates4 = list( df4[ df4.columns[3] ] )
 bias_estimates5 = list( df5[ df5.columns[3] ] )
 bias_estimates6 = list( df6[ df6.columns[3] ] )
-bias_estimates7 = list( df7[ df7.columns[3] ] )
 
-bias_estimates = bias_estimates1 + bias_estimates2 + bias_estimates3 + bias_estimates4 + bias_estimates5 + bias_estimates6 + bias_estimates7
+bias_estimates = bias_estimates1 + bias_estimates2 + bias_estimates3 + bias_estimates4 + bias_estimates5 + bias_estimates6
 
 # COMPUTE + CONCATENATE: 95% c.i. uncertainty
 
@@ -123,7 +115,6 @@ bias_estimates3_025 = df3[ df3.columns[4] ]
 bias_estimates4_025 = df4[ df4.columns[4] ]
 bias_estimates5_025 = df5[ df5.columns[4] ]
 bias_estimates6_025 = df6[ df6.columns[4] ]
-bias_estimates7_025 = df7[ df7.columns[4] ]
 
 bias_estimates1_975 = df1[ df1.columns[5] ]
 bias_estimates2_975 = df2[ df2.columns[5] ]
@@ -131,7 +122,6 @@ bias_estimates3_975 = df3[ df3.columns[5] ]
 bias_estimates4_975 = df4[ df4.columns[5] ]
 bias_estimates5_975 = df5[ df5.columns[5] ]
 bias_estimates6_975 = df6[ df6.columns[5] ]
-bias_estimates7_975 = df7[ df7.columns[5] ]
               
 bias_uncertainty1 = list( np.subtract( bias_estimates1_975, bias_estimates1_025 ) )
 bias_uncertainty2 = list( np.subtract( bias_estimates2_975, bias_estimates2_025 ) )
@@ -139,9 +129,8 @@ bias_uncertainty3 = list( np.subtract( bias_estimates3_975, bias_estimates3_025 
 bias_uncertainty4 = list( np.subtract( bias_estimates4_975, bias_estimates4_025 ) )
 bias_uncertainty5 = list( np.subtract( bias_estimates5_975, bias_estimates5_025 ) )
 bias_uncertainty6 = list( np.subtract( bias_estimates6_975, bias_estimates6_025 ) )
-bias_uncertainty7 = list( np.subtract( bias_estimates7_975, bias_estimates7_025 ) )
 
-bias_uncertainties = bias_uncertainty1 + bias_uncertainty2 + bias_uncertainty3 + bias_uncertainty4 + bias_uncertainty5 + bias_uncertainty6 + bias_uncertainty7
+bias_uncertainties = bias_uncertainty1 + bias_uncertainty2 + bias_uncertainty3 + bias_uncertainty4 + bias_uncertainty5 + bias_uncertainty6
                
 # Index(['index', 'stationcode', 'exposure_category', 'bias_estimate', 'bias_estimate_2.5', 'bias_estimate_97.5', 'source_flag', 'exposurecorrected_flag'], dtype='object')
 
@@ -153,9 +142,8 @@ source_flags3 = list( df3[ df3.columns[6] ] )
 source_flags4 = list( df4[ df4.columns[6] ] )
 source_flags5 = list( df5[ df5.columns[6] ] )
 source_flags6 = list( df6[ df6.columns[6] ] )
-source_flags7 = list( df7[ df7.columns[6] ] )
 
-source_flags = source_flags1 + source_flags2 + source_flags3 + source_flags4 + source_flags5 + source_flags6 + source_flags7
+source_flags = source_flags1 + source_flags2 + source_flags3 + source_flags4 + source_flags5 + source_flags6
 
 # CONCATENATE: exposurecorrected_flag
 
@@ -165,14 +153,16 @@ exposurecorrected_flags3 = list( df3[ df3.columns[7] ] )
 exposurecorrected_flags4 = list( df4[ df4.columns[7] ] )
 exposurecorrected_flags5 = list( df5[ df5.columns[7] ] )
 exposurecorrected_flags6 = list( df6[ df6.columns[7] ] )
-exposurecorrected_flags7 = list( df7[ df7.columns[7] ] )
 
-exposurecorrected_flags = exposurecorrected_flags1 + exposurecorrected_flags2 + exposurecorrected_flags3 + exposurecorrected_flags4 + exposurecorrected_flags5 + exposurecorrected_flags6 + exposurecorrected_flags7
+exposurecorrected_flags = exposurecorrected_flags1 + exposurecorrected_flags2 + exposurecorrected_flags3 + exposurecorrected_flags4 + exposurecorrected_flags5 + exposurecorrected_flags6 
 
 # CONSTRUCT: dataframe
 
 df = pd.DataFrame( {'datetime':datetimes, 'stationcode':stationcodes, 'exposure_category':exposure_categories, 'bias':bias_estimates, 
                     'uncertainty':bias_uncertainties, 'source_flag':source_flags, 'exposurecorrected_flag':exposurecorrected_flags } )
+                    
+# SAVE: dataframe
+                    
 df.to_pickle( exposure_bias_model_file, compression='bz2' )
 
 #------------------------------------------------------------------------------
